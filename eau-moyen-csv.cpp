822f2406e25fb1241d340100838da9206f42a73a
Gad3lan <alan.adamiak@gmail.com>
2018-11-27 12:32:18 +0100

    Realisation du main d'eau moyen csv

--- eau-moyen-csv.cpp
+++ eau-moyen-csv.cpp
@@ -6,6 +6,19 @@
 using namespace std;
 
 int main() {
-    // Remplacer la ligne suivante par le code adéquat
-    throw runtime_error("Fonction main non implanté ligne 10");
+	ifstream donnees("donnees/volumes_d_eau_distribues.csv");
+	string entete;
+	getline(donnees, entete);
+	
+	string mois, volume_str;
+	int vol_moyen;
+	int vol_total = 0;
+	while (getline(donnees, mois, ';') && getline(donnees, volume_str)) {
+		if ( volume_str.size() > 0 and volume_str[volume_str.length()-1] == '\r' )
+			volume_str.resize(volume_str.length() - 1);
+		vol_total += stoi(volume_str);
+	}
+	vol_moyen = vol_total  / 12;
+	cout << vol_moyen;
+	return 1;
 }
