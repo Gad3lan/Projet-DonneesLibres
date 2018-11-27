a11bddc0dacb8c25fba72c81001791aadc5ffdff
Willixpop <45388593+Willixpop@users.noreply.github.com>
2018-11-27 12:18:44 +0100

    changement eau total avec lecteur csv

--- eau-total-csv.cpp
+++ eau-total-csv.cpp
@@ -3,9 +3,20 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
+#inculde <string>
 using namespace std;
 
 int main() {
-    // Remplacer la ligne suivante par le code adéquat
-    throw runtime_error("Fonction main non implanté ligne 10");
+    ifstream donnees("donnes/volumes_d_eau_distribues.csv");
+    string entete;
+    getline(donnees, entete);
+	
+	
+	 string mois, string volume_str;
+	 int total_volume = 0;
+     while(getline(fichier, mois, ';') && getline(fichier, volume_str))
+          total_volume += stoi(volume_str);
+     cout << "Le volume total d'eau distribué de cette année est de : " << total_volume << "L" << endl;
+     return 1;
 }
+
