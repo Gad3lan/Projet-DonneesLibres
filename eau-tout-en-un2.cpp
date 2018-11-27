cd60d68756b4ae9f469de5b6896cdfd4aba6f5f3
Gad3lan <alan.adamiak@gmail.com>
2018-11-27 11:44:32 +0100

    Resolution bug eau tout en un 2

--- eau-tout-en-un2.cpp
+++ eau-tout-en-un2.cpp
@@ -11,9 +11,9 @@ using namespace std;
  * - le mois avec le plus gros volume d'eau et son volume
  **/
 int main() {
-    vector<vector<int>> t = litTableau("donnees/doc.txt", 2);
+    vector<vector<int>> t = litTableau("donnees/volumes_d_eau_distribues.txt", 2);
     vector<int> valeurs = colonne(t, 1);
-    string mois = {"Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre"};
+    string mois[12] = {"Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre"};
     cout << "Le volume d´eau total sur l´année est de : " << somme(valeurs) << endl << "La moyenne d´eau utilisée par mois est de : " << moyenne(valeurs) << endl << "Le mois avec le plus gros volume d´eau est << mois[t[0][indiceMax(valeurs)] - 1] << " avec " << t[1][indiceMax(valeurs)] << " litres." << endl;
 	return 1;
 }
